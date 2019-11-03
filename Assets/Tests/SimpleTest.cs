678b79b0b01e839226653385aa796f895effea3f
Kevin Turcumani <kevinjaja98@gmail.com>
2019-11-04 10:11:19 +1100

    Release

---
+++ Assets/Tests/SimpleTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NUnit.Framework;
+using UnityEngine.TestTools;
+public class SimpleTest
+{
+    [Test]   
+    public void NUnitTest()
+    {
+        Assert.IsTrue(true, "NUnit Test Failed");
+    }
+
+    [UnityTest]
+    public IEnumerator UnityTest()
+    {
+        yield return new WaitForFixedUpdate();
+
+        Assert.IsTrue(true, "Unity Test Failed");
+    }
+}
